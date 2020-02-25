571cb7758a484f59de30eedbe7306560d2978618
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-02-25 16:27:14 +1100

    Committing before I use express-jwt

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const express = require('express');
+const jwt = require('jwt-simple');
 const app = express();
-const port = 3000;
+const port = 8080;
+
+const config = require('./config.json');
+
+function check_jwt(aafjwt) {
+
+	if( aafjwt['iss'] !== config['iss'] || aafjwt['aud'] !== config['aud'] ) {
+		return false;
+	}
+
+	const nbf = new Date(aafjwt['nbf']);
+	const exp = new Date(aafjwt['exp']);
+	const now = new Date();
+
+	return ( now > nbf && now < exp );
+
+}
 
 
-app.get('/yes', (req, res) => {
-	console.log(JSON.stringify(req.headers));
-	res.send('success')
+
+app.get('/', (req, res) => {
+	res.redirect(302, config['aafURL']);
 });
+
+
 app.get('/no', (req, res) => res.sendStatus(403) )
 
-app.listen(port, '0.0.0.0', () => console.log(`Auth-o-matic on ${port}`));
+app.post('/jwt', (req, res) => {
+	console.log(JSON.stringify(req.params));
+	const aafjwt = jwt.decode(req.params['assertion'], config['secret']);
+
+	if( check_jwt(aafjwt) ) {
+		res.send('AAF authentication worked');
+	} else {
+		res.sendStatus(403);
+	}
+
+})
+
+
+
+
+app.listen(port, '0.0.0.0', () => console.log(`express-aaf listening on ${port}`));
